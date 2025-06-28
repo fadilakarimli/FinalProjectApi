566a1aaa1903d1ec74beff41e1442c27fe5e4e61
fadilakarimli <fadilafk@code.edu.az>
2025-06-28 19:07:01 +0400

    added something for review and contact

--- FinalProject/FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3a2c321b65a11a760f6e3c6cb7e50d1e607ec39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+729f43386ab72363453af351625543ac83143593")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
